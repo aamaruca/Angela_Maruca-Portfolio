262545ee8af3b5b0f9056e699ab1ef4653fa2495
Angela Maruca <angelamaruca@Angelas-MacBook-Pro.local>
2021-08-02 12:00:35 -0400

    created base files, found fonts, colors and images

---
+++ projects.js
@@ -0,0 +1,23 @@
+const projects = [
+  {
+    title: "The Sssavy Sssipper",
+    img_url1: "https://i.imgur.com/LFRKNPtl.jpg?1",
+    description: "The Savvy Sipper is be an app where users can find recipes and ingredients for cocktails."
+  },
+  {
+    title: "GrowMe",
+    img_url: "https://i.imgur.com/VBdF6Iel.jpg?1",
+    description: "GrowMe is a note taking app for gardeners. It will allow a user to enter in information about the individual plants they have and  make notes on that particular plant so they can keep track of them as the season progresses."
+  },
+  {
+    title: "AKEA Comics",
+    img_url: "https://i.imgur.com/nycDeG1l.jpg?1",
+    description: "AKEA-Comics is a mobile first web-app that allows comic book fans to browse an inventory of Marvel and DC comic books for sale. Once a user signs up for an account they will be given access to adding their own comic books for sale, along with editing and deleting posts. Wishlist functionality will also be available to users."
+  },
+  {
+    title: "Play, Learn, Share!",
+    img_url: "https://i.imgur.com/X3wL5WEl.png?1",
+    description: "Play-learn-share is an app that consolidates ideas for at home activities for children ages 2-5. It's purpose is to make it easier for parents to find activities that are age and developmentally appropriate for their kids. Users will be able to create posts of the various activities they've tried and have found to be succesful. In short it's an app to help parents play with their kids, watch them learn through this play and share their ideas!"
+  }
+  
+]
