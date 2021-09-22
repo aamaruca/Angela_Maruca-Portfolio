55626598dfdfe53f682a605914147d3a0159998e
aamaruca <aahalling@gmail.com>
2021-09-22 11:52:29 -0400

    edited project photo

--- projects.js
+++ projects.js
@@ -2,30 +2,33 @@ const projects = [
   {
     title: "The Sssavy Sssipper",
     img_url: "https://i.imgur.com/LFRKNPtl.jpg?1",
-    description: "Built with Javascript, CSS and HTML The Sssavvy Sssipper consumes a third party API. It is an app for users to find recipes and ingredients for cocktails. ",
+    description:
+      "Built with Javascript, CSS and HTML The Sssavvy Sssipper consumes a third party API. It is an app for users to find recipes and ingredients for cocktails. ",
     url: "https://aamaruca.github.io/The-Sssavvy-Sssipper/",
-    github: "https://github.com/aamaruca/The-Sssavvy-Sssipper"
+    github: "https://github.com/aamaruca/The-Sssavvy-Sssipper",
   },
   {
     title: "GrowMe",
     img_url: "https://i.imgur.com/VBdF6Iel.jpg?1",
-    description: "GrowMe is a full CRUD React App. Utilizing Airtable and Cloudinary as back-end custom API's. It will allow a user to enter in information about the individual plants they have and  make notes on that particular plant so they can keep track of them as the season progresses.",
+    description:
+      "GrowMe is a full CRUD React App. Utilizing Airtable and Cloudinary as back-end custom API's. It will allow a user to enter in information about the individual plants they have and  make notes on that particular plant so they can keep track of them as the season progresses.",
     url: "https://grow-me.netlify.app/",
-    github: "https://github.com/aamaruca/GrowMe"
+    github: "https://github.com/aamaruca/GrowMe",
   },
   {
     title: "AKEA Comics",
     img_url: "https://i.imgur.com/nycDeG1l.jpg?1",
-    description: "AKEA-Comics was a collaborative project. A mobile first web-app that allows comic book fans to browse an inventory of Marvel and DC comic books for sale. Once a user signs up for an account they will be given access to adding their own comic books for sale, along with editing, deleting posts and a utilizing a wishlist feature.",
+    description:
+      "AKEA-Comics was a collaborative project. A mobile first web-app that allows comic book fans to browse an inventory of Marvel and DC comic books for sale. Once a user signs up for an account they will be given access to adding their own comic books for sale, along with editing, deleting posts and a utilizing a wishlist feature.",
     url: "https://akea-comics.netlify.app",
-    github: "https://github.com/aamaruca/AKEA-Comics"
+    github: "https://github.com/aamaruca/AKEA-Comics",
   },
   {
     title: "Play, Learn, Share!",
-    img_url: "https://i.imgur.com/X3wL5WEl.png?1",
-    description: "Play-learn-share is an app that consolidates ideas for at home activities for children ages 2-5. Users will be able to create posts of the various activities they've tried and have found to be succesful. In short it's an app to help parents play with their kids, watch them learn through this play and share their ideas!",
+    img_url: "https://i.imgur.com/hT8pHpt.png",
+    description:
+      "Play-learn-share is an app that consolidates ideas for at home activities for children ages 2-5. Users will be able to create posts of the various activities they've tried and have found to be succesful. In short it's an app to help parents play with their kids, watch them learn through this play and share their ideas!",
     url: "https://play-learn-share.netlify.app/ ",
-    github: "https://github.com/aamaruca/play-learn-share"
-  }
-  
-]
+    github: "https://github.com/aamaruca/play-learn-share",
+  },
+];
